616c76ea4684eb99d2dfab8e1c21a5ed73230f92
Aditya Sahni <95126292+sahniaditya007@users.noreply.github.com>
2025-01-30 02:33:26 +0530

    implement UserService with CRUD operations

---
+++ src/services/UserService.js
@@ -0,0 +1,37 @@
+import axios from 'axios';
+
+const API_URL = 'https://jsonplaceholder.typicode.com/users';
+
+export default {
+    async getUsers() {
+        try {
+            const response = await axios.get(API_URL);
+            return response.data;
+        } catch (error) {
+            throw new Error('Failed to fetch users');
+        },
+        async createUser(userdata){
+            try {
+                const response = await axios.post(API_URL, userdata);
+                return response.data;
+            } catch (error) {
+                throw new Error('User creation failed');
+            }
+        },
+        async updateUser(UserId, updateData){
+            try {
+                const response = await axios.put('${API_URL}/${userId}', updatedData);
+                return response.data;
+            } catch (error) {
+                throw new Error('User update failed');
+            }
+        },
+        asysnc deleteUser(userId){
+            try {
+                await axios.delete('${API_URL}/${userId}');
+            } catch (error) {
+                throw new Error ('User deletion failed');
+            }
+        }
+    }
+}
